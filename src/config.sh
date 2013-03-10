f7cec279df52b84dbdba6c61beafc8cb4467932a
Jean-Charles Lefebvre <polyvertex@gmail.com>
2013-03-10 10:41:17 +0100

    * More minimalistic configuration required (i.e.: only SVN repository URL) * Cleaner upgrade process

--- src/config.sh
+++ src/config.sh
@@ -8,11 +8,9 @@
 
 # configuration
 SVN_REPOSITORY_URL="https://svn.jcl.io/pmon/trunk/src/"
-PID_FILE="/tmp/pmon_install.pid"
-TMP_FILE="/tmp/pmon_install.tmp"
-TMP_DIR="/tmp/pmon_install_dir"
 
 
+#-------------------------------------------------------------------------------
 # get the real path of this script
 THIS_SCRIPT=${BASH_SOURCE[0]}
 while [ -h "$THIS_SCRIPT" ]; do # resolve $THIS_SCRIPT until the file is no longer a symlink
@@ -25,13 +23,17 @@ done
 THIS_SCRIPT_DIR=$(cd -P "$(dirname "$THIS_SCRIPT")" && pwd)
 THIS_SCRIPT_NAME=$(basename "$THIS_SCRIPT")
 
-# global variables
+# global parameters
 ACTION=""
 REVISION=""
 INSTALL_DIR=""
 INSTALL_AGENT=0
 INSTALL_DAEMON=0
 
+# global variables
+TMP_DIR=""
+TMP_DIR_SVNCOPY=""
+TMP_FILE=""
 
 
 #-------------------------------------------------------------------------------
@@ -40,16 +42,16 @@ function usage()
     echo "Usage:"
     echo ""
     echo "* $THIS_SCRIPT_NAME install-all [install_dir] [revision]"
-    echo "  To install or reinstall the PMon Daemon (server) and the PMon Agent"
+    echo "  To install or update the PMon Daemon (server) and the PMon Agent"
     echo "  altogether in the specified directory or, by default, in the same"
     echo "  directory than this scripts."
     echo ""
     echo "* $THIS_SCRIPT_NAME install-agent [install_dir] [revision]"
-    echo "  To install or reinstall the PMon Agent in the specified directory or,"
+    echo "  To install or update the PMon Agent in the specified directory or,"
     echo "  by default, in the same directory than this script."
     echo ""
     echo "* $THIS_SCRIPT_NAME install-daemon [install_dir] [revision]"
-    echo "  To install or reinstall the PMon Daemon (server) in the specified"
+    echo "  To install or update the PMon Daemon (server) in the specified"
     echo "  directory or, by default, in the same directory than this script."
     echo ""
 }
@@ -57,8 +59,7 @@ function usage()
 #-------------------------------------------------------------------------------
 function cleanup()
 {
-    [ -e "$TMP_FILE" ] && rm -f "$TMP_FILE"
-    [ -e "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+    [ -n "$TMP_DIR" -a  -e "$TMP_DIR" ] && rm -rf "$TMP_DIR"
 }
 
 #-------------------------------------------------------------------------------
@@ -106,9 +107,23 @@ function die()
 #}
 
 #-------------------------------------------------------------------------------
+function init_vars()
+{
+    [ -z "$TMP_DIR" ] && TMP_DIR=$(mktemp -d)
+    [ -n "$TMP_DIR" -a -d "$TMP_DIR" ] || \
+        die 1 "Failed to create temp directory!"
+
+    TMP_FILE="$TMP_DIR/tmp"
+    touch "$TMP_FILE" || die 1 "Failed to create temp file!"
+    rm "$TMP_FILE"
+
+    TMP_DIR_SVNCOPY="$TMP_DIR/svncopy"
+}
+
+#-------------------------------------------------------------------------------
 function svn_get()
 {
-    [ -e "$TMP_DIR/svnexport" ] || mkdir -p "$TMP_DIR/svnexport"
+    [ -e "$TMP_DIR_SVNCOPY" ] || mkdir -p "$TMP_DIR_SVNCOPY"
 
     # export content of the svn repository
     echo "Fecthing SVN copy from $SVN_REPOSITORY_URL (rev $REVISION)..."
@@ -117,7 +132,7 @@ function svn_get()
     svn export \
         --force \
         --revision $REVISION \
-        "$SVN_REPOSITORY_URL" "$TMP_DIR/svnexport" > "$TMP_FILE"
+        "$SVN_REPOSITORY_URL" "$TMP_DIR_SVNCOPY" > "$TMP_FILE"
     [ $? -eq 0 ] || die 1 "Failed to fetch SVN copy!"
     echo
 
@@ -128,11 +143,25 @@ function svn_get()
     echo "Exported revision $REVISION."
 
     # keep trace of the revision number
-    echo "$REVISION" > "$TMP_DIR/svnexport/.revision"
-    date '+%Y-%m-%d %H:%M:%S' > "$TMP_DIR/svnexport/.timestamp"
+    echo "$REVISION" > "$TMP_DIR_SVNCOPY/.revision"
+    date '+%Y-%m-%d %H:%M:%S' > "$TMP_DIR_SVNCOPY/.timestamp"
 
     echo
 }
+
+#-------------------------------------------------------------------------------
+#function fork_install()
+#{
+#    local configscript="$TMP_DIR_SVNCOPY/config.sh"
+#
+#    [ -e "$configscript" ] || die 1 "Install script not found in \"$configscript\"!"
+#    [ -x "$configscript" ] || die 1 "Install script is not executable \"$configscript\"!"
+#    #touch "$FORKED_FILE"
+#    #[ -e "$FORKED_FILE" ] || die 1 "Could not create the 'forked' file at \"$FORKED_FILE\"!"
+#
+#    exec "$configscript" "$ACTION" "$INSTALL_DIR" "$REVISION"
+#    exit 0
+#}
 
 #-------------------------------------------------------------------------------
 function pre_install()
@@ -175,40 +204,43 @@ function do_install()
     local first_install_daemon=1
     local first_install_agent=1
     local tmp
+    local dir_scripts_avail="$INSTALL_DIR/etc/scripts-available"
+    local dir_scripts_daily="$INSTALL_DIR/etc/scripts-daily"
+    local dir_scripts_hourly="$INSTALL_DIR/etc/scripts-hourly"
+    local dir_scripts_minute="$INSTALL_DIR/etc/scripts-minute"
 
-    # backup user's files
-    [ -e "$INSTALL_DIR/etc" ] && mv "$INSTALL_DIR/etc" "$TMP_DIR/"
+    # can we overwrite install dir?
+    [ -d "$INSTALL_DIR" -a -w "$INSTALL_DIR" ] || \
+        die 1 "Cannot write into \"$INSTALL_DIR\"!"
 
-    # cleanup destination directory first
-    rm -rf "$INSTALL_DIR/*" "$INSTALL_DIR/.*" || \
-        die 1 "Failed to cleanup $INSTALL_DIR!"
+    # first install?
+    [ -e "$INSTALL_DIR/bin/pmond.pl" ] && first_install_daemon=0
+    [ -e "$INSTALL_DIR/bin/pmona.pl" ] && first_install_agent=0
 
-    # --- install ---
+    # cleanup destination directory (do not 'set -e' here!)
+    rm -f "$INSTALL_DIR/etc/*.dist" &> /dev/null
+    rm -f "$INSTALL_DIR/var/*.pid" &> /dev/null
+    mv -f "$INSTALL_DIR/var/pmond.log" "$INSTALL_DIR/var/pmond.log.1" &> /dev/null
+    rm -rf "$INSTALL_DIR/bin" &> /dev/null
 
     # install config script
-    mv -f "$TMP_DIR/svnexport/config.sh" "$INSTALL_DIR/"
+    #mv -f "$TMP_DIR_SVNCOPY/config.sh" "$INSTALL_DIR/"
 
     # install revision files
-    mv -f "$TMP_DIR/svnexport/.revision" "$INSTALL_DIR/"
-    mv -f "$TMP_DIR/svnexport/.timestamp" "$INSTALL_DIR/"
-
-    # restore user's files that were originally installed if needed
-    if [ -e "$TMP_DIR/etc" ]; then
-        [ -e "$TMP_DIR/etc/pmond.conf" ] && first_install_daemon=0
-        [ -e "$TMP_DIR/etc/pmona.conf" ] && first_install_agent=0
-        mv -f "$TMP_DIR/etc" "$INSTALL_DIR/"
-    fi
+    mv -f "$TMP_DIR_SVNCOPY/.revision" "$INSTALL_DIR/"
+    mv -f "$TMP_DIR_SVNCOPY/.timestamp" "$INSTALL_DIR/"
 
     # create directories structure
     [ -e "$INSTALL_DIR/bin" ] || mkdir -p "$INSTALL_DIR/bin"
     [ -e "$INSTALL_DIR/etc" ] || mkdir -p "$INSTALL_DIR/etc"
     [ -e "$INSTALL_DIR/var" ] || mkdir -p "$INSTALL_DIR/var"
+    [ -e "$INSTALL_DIR/bin" ] || die 1 "Failed to create main directories structure in $INSTALL_DIR!"
     if [ $INSTALL_AGENT -ne 0 ]; then
-        [ -e "$INSTALL_DIR/etc/scripts-available" ] || mkdir "$INSTALL_DIR/etc/scripts-available"
-        [ -e "$INSTALL_DIR/etc/scripts-daily" ] || mkdir "$INSTALL_DIR/etc/scripts-daily"
-        [ -e "$INSTALL_DIR/etc/scripts-hourly" ] || mkdir "$INSTALL_DIR/etc/scripts-hourly"
-        [ -e "$INSTALL_DIR/etc/scripts-minute" ] || mkdir "$INSTALL_DIR/etc/scripts-minute"
-        [ -e "$INSTALL_DIR/etc/scripts-minute" ] || die 1 "Failed to create directories in $INSTALL_DIR/etc!"
+        [ -e "$dir_scripts_avail" ] || mkdir "$dir_scripts_avail"
+        [ -e "$dir_scripts_daily" ] || mkdir "$dir_scripts_daily"
+        [ -e "$dir_scripts_hourly" ] || mkdir "$dir_scripts_hourly"
+        [ -e "$dir_scripts_minute" ] || mkdir "$dir_scripts_minute"
+        [ -e "$dir_scripts_minute" ] || die 1 "Failed to create directories in $INSTALL_DIR/etc!"
     fi
 
     # install config files
@@ -217,54 +249,58 @@ function do_install()
     [ $INSTALL_DAEMON -ne 0 ] && tmp="$tmp pmond"
     if [ -n "$tmp" ]; then
         for name in $tmp; do
-            if [ -e "$INSTALL_DIR/etc/$name.conf" ]; then
-                mv -f "$TMP_DIR/svnexport/$name.sample.conf" "$INSTALL_DIR/etc/$name.conf.dist"
+            local srcfile="$TMP_DIR_SVNCOPY/$name.sample.conf"
+            local destfile="$INSTALL_DIR/etc/$name.conf"
+
+            if [ -e "$destfile" ]; then
+                mv -f "$srcfile" "${destfile}.dist"
             else
-                mv "$TMP_DIR/svnexport/$name.sample.conf" "$INSTALL_DIR/etc/$name.conf"
-                [ -e "$INSTALL_DIR/etc/$name.conf.dist" ] && rm -f "$INSTALL_DIR/etc/$name.conf.dist"
+                mv "$srcfile" "$destfile"
+                [ -e "${destfile}.dist" ] && rm -f "${destfile}.dist"
             fi
         done
     fi
 
-    # install scripts in the 'available' directory
+    # install agent's scripts
     if [ $INSTALL_AGENT -ne 0 ]; then
-        for srcfile in $TMP_DIR/svnexport/scripts/*; do
-            local destfile="$INSTALL_DIR/etc/scripts-available/$(basename "$srcfile")"
-            #if [ -e "$destfile" ]; then
-            #    cmp_files "$srcfile" "$destfile"
-            #    [ $? -ne 0 ] && destfile="$destfile.dist"
-            #fi
+        for srcfile in $TMP_DIR_SVNCOPY/scripts/*; do
+            local destfile="$dir_scripts_avail/$(basename "$srcfile")"
             mv -f "$srcfile" "$destfile"
+            chmod 0750 "$destfile"
         done
 
-        # if it is the first time we install, create default links to the
-        # scripts we want to run
-        if [ 1 ]; then  #$first_install_agent -ne 0 ]; then
-            local dir_avail="$INSTALL_DIR/etc/scripts-available"
-            local dir_daily="$INSTALL_DIR/etc/scripts-daily"
-            local dir_hourly="$INSTALL_DIR/etc/scripts-hourly"
-            local dir_minute="$INSTALL_DIR/etc/scripts-minute"
+        pushd "$dir_scripts_daily" > /dev/null || die 1 "Failed to cd to \"$dir_scripts_daily\"!"
+        for name in system.sh; do
+            ln -sf "../scripts-available/$name" "$name"
+        done
+        popd > /dev/null
 
-            ln -sf "$dir_avail/smart.pl" "$dir_hourly/"
-            ln -sf "$dir_avail/system.sh" "$dir_daily/"
-            ln -sf "$dir_avail/usage.pl" "$dir_minute/"
-        fi
+        pushd "$dir_scripts_hourly" > /dev/null || die 1 "Failed to cd to \"$dir_scripts_hourly\"!"
+        for name in smart.pl; do
+            ln -sf "../scripts-available/$name" "$name"
+        done
+        popd > /dev/null
+
+        pushd "$dir_scripts_minute" > /dev/null || die 1 "Failed to cd to \"$dir_scripts_minute\"!"
+        for name in usage.pl; do
+            ln -sf "../scripts-available/$name" "$name"
+        done
+        popd > /dev/null
     fi
 
-    # install agent's perl files
+    # install agent's binary files
     if [ $INSTALL_AGENT -ne 0 ]; then
-        [ -e "$INSTALL_DIR/bin" ] || mkdir -p "$INSTALL_DIR/bin"
         for fname in pmona.pl; do
-            mv -f "$TMP_DIR/svnexport/$fname" "$INSTALL_DIR/bin/"
+            mv -f "$TMP_DIR_SVNCOPY/$fname" "$INSTALL_DIR/bin/"
         done
         chmod 0750 "$INSTALL_DIR/bin/pmona.pl"
     fi
 
-    # install daemon's perl files
+    # install daemon's binary files
     if [ $INSTALL_DAEMON -ne 0 ]; then
         for fname in PMon pmond.pl pmond.sh; do
             [ -d "$INSTALL_DIR/bin/$fname" ] && rm -rf "$INSTALL_DIR/bin/$fname"
-            mv -f "$TMP_DIR/svnexport/$fname" "$INSTALL_DIR/bin/"
+            mv -f "$TMP_DIR_SVNCOPY/$fname" "$INSTALL_DIR/bin/"
         done
         chmod 0750 "$INSTALL_DIR/bin/pmond.pl"
         chmod 0750 "$INSTALL_DIR/bin/pmond.sh"
@@ -281,6 +317,13 @@ function do_install()
 
 
 #-------------------------------------------------------------------------------
+for cmd in which basename bash cat chmod chown cut date dirname head grep ln mktemp mv readlink rm stat svn touch tr; do
+    which $cmd &> /dev/null
+    [ $? -eq 0 ] || die 1 "Required command '$cmd' not found!"
+done
+
+# do not forget to modify the fork_install() function according to your
+# changes on global parameters here!
 ACTION="$1"
 INSTALL_DIR="$2"
 REVISION="$3"
@@ -289,43 +332,25 @@ REVISION="$3"
 [ -z "$INSTALL_DIR" ] && INSTALL_DIR="$THIS_SCRIPT_DIR"
 [ -z "$REVISION" ] && REVISION="HEAD"
 
-for cmd in which basename bash cat chmod chown cut date dirname head grep ln mv readlink rm stat svn touch tr; do
-    which $cmd &> /dev/null
-    [ $? -eq 0 ] || die 1 "Required command '$cmd' not found!"
-done
-
-#if [ -e "$THIS_SCRIPT_DIR/.devmode" ]; then
-#    cleanup
-#    svn_get
-#elif [ -z "$PMON_CONFIG_BOOSTRAPPED_FROM" ]; then
-#    cleanup
-#    svn_get
-#    s="$TMP_DIR/svnexport/config.sh"
-#    [ -e "$s" ] || die 1 "Could not find config script $s!"
-#    chmod 0750 "$s"
-#    PMON_CONFIG_BOOSTRAPPED_FROM="$THIS_SCRIPT" bash -- "$s" $*
-#    exit $?
-#fi
-
 case "$ACTION" in
     install-all)
         INSTALL_AGENT=1
         INSTALL_DAEMON=1
-        cleanup
+        init_vars
         svn_get
         pre_install
         do_install
         ;;
     install-agent)
         INSTALL_AGENT=1
-        cleanup
+        init_vars
         svn_get
         pre_install
         do_install
         ;;
     install-daemon)
         INSTALL_DAEMON=1
-        cleanup
+        init_vars
         svn_get
         pre_install
         do_install
